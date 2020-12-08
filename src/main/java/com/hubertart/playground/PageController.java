c05188f6bb6dbc6cea972df64b2f5df823595248
fallensteed <scott@hubertart.com>
2020-12-08 14:25:01 -0700

    Prior to refactor

--- src/main/java/com/hubertart/playground/PageController.java
+++ src/main/java/com/hubertart/playground/PageController.java
@@ -1,25 +1,64 @@
 package com.hubertart.playground;
 
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PageController {
+
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
     }
+
     @GetMapping("/tasks")
     public String getTasks() {
         return "These are tasks";
     }
+
     @PostMapping("/tasks")
     public String createTask(){
         return "You just POSTed to /tasks";
     }
+
     @GetMapping("/math/pi")
     public String getPie(){
         return "" + Math.PI;
+    }
+
+    @GetMapping("/math/calculate")
+    public String performOperations(
+            @RequestParam(value="operation", required=false, defaultValue = "add") String mathType,
+            @RequestParam("x") Integer x,
+            @RequestParam("y") Integer y
+    ) {
+        switch (mathType) {
+            case "multiply":
+                return (x + " * " + y + " = " + (x * y));
+            case "subtract":
+                return (x + " - " + y + " = " + (x - y));
+            case "divide":
+                return (x + " / " + y + " = " + (x / y));
+            default:
+                return (x + " + " + y + " = " + (x + y));
+        }
+    }
+
+    @PostMapping("/math/sum")
+    public String performSummation(
+            @RequestParam Integer [] n){
+
+        int total = 0;
+        String output = "";
+        for( int i : n){
+            output += i + " + ";
+            total += i;
+        }
+        return output.substring(0, output.length() - 2) + "= " + total;
     }
 }
--- src/test/java/com/hubertart/playground/PageController.java
+++ src/test/java/com/hubertart/playground/PageController.java
@@ -32,6 +32,48 @@ class PageControllerTests {
                 .andExpect(content().string("3.141592653589793"));
     }
 
+    @Test
+    public void testMathCalculateAddition4And6Eq10() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=add&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testMathCalculateMultiply4And6Eq24() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=multiply&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testMathCalculateSubtract4And6EqNeg2() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=subtract&x=4&y=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testMathCalculateDivide30And5Eq6() throws Exception {
+        this.mvc.perform(get("/math/calculate?operation=divide&x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
+    @Test
+    public void testMathCalculate30And5Eq35() throws Exception {
+        this.mvc.perform(get("/math/calculate?x=30&y=5").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+    }
+
+    @Test
+    public void testMathSumAdd4And5And6Eq15() throws Exception {
+        this.mvc.perform(post("/math/sum?n=4&n=5&n=6").accept(MediaType.TEXT_PLAIN))
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 5 + 6 = 15"));
+    }
+
 //    @Test
 //    public void testPostMessageEndpoint() throws Exception {
 //        this.mvc.perform(post("/messages"))
